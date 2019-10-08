9868bb3ef94fa2308edb1011cce40e8e71ee7d04
Araozu <tbyuyfcjjll@gmail.com>
2019-10-08 16:14:55 -0500

    Agregada funcion impr que imprime en consola en modo DEV.

--- src/variables.js
+++ src/variables.js
@@ -2,3 +2,6 @@ export const servidor = process.env.NODE_ENV === "production"?
     "https://araozu.pro":
     "";
 // export const servidor = "https://araozu.pro";
+
+export const impr = process.env.NODE_ENV !== "production"? window.console.log: () => {};
+
