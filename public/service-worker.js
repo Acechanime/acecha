5df5ac00742c4ae04940db5d5f2c94549f511af8
Araoz (Moq) <faraoz@unsa.edu.pe>
2020-02-27 19:00:21 -0500

    Agregadas reglas al service-worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,6 +25,9 @@ self.addEventListener("install", (event) => {
 
 
 self.addEventListener('fetch', (event) => {
+    if (event.request.method != 'GET') return;
+    if (event.request.url.find("api") !== -1) return;
+
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
