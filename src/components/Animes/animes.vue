bb56eb55d68696cc8e07dff5716533f32da40ba3
Araozu <tbyuyfcjjll@gmail.com>
2020-03-12 18:03:51 -0500

    Agregado codigo necesario para la paginacion de /animes/

--- src/components/Animes/animes.vue
+++ src/components/Animes/animes.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
-    div.gridIn
-        anime(v-for="anime in listaAnimesFiltrada2" :key="anime.anime_id" :anime="anime")
+    div
+        // p {{ numeroDeAnimes }}
+        div.gridIn
+            anime(v-for="anime in listaAnimesFiltrada3" :key="anime.anime_id" :anime="anime")
     //
 </template>
 
@@ -8,6 +10,7 @@
     import anime from "./anime.vue"
     import { comp, filtrar } from "./buscador.coffee"
 
+    numeroDeFilas = 4
 
     export default
         name: "animes"
@@ -24,14 +27,26 @@
                 type: Function
                 required: true
         computed:
-            listaAnimes: -> @$store.state.datos.listaAnimes
-            listaAnimesFiltrada2: ->
-                if @listaAnimes isnt undefined
-                    @terminarCargaFn()
-                    primerRes = filtrar @, @listaAnimes, @filtro
-                    primerRes.sort @funOrden
-
+            listaAnimes: ->
+                if @$store.state.datos.listaAnimes isnt undefined
+                    @$store.state.datos.listaAnimes
                 else []
+            listaAnimesFiltrada2: ->
+                @terminarCargaFn()
+                primerRes = filtrar @, @listaAnimes, @filtro
+                primerRes.sort @funOrden
+            anchoPantalla: ->
+                @$store.state.datos.resizeEvent
+                window.innerWidth
+            numeroDeAnimes: ->
+                ancho = @anchoPantalla
+                numeroDeColumnas = do ->
+                    if ancho > 1440 then 5
+                    else if ancho > 500 then 4
+                    else 2
+                numeroDeFilas * numeroDeColumnas
+            listaAnimesFiltrada3: -> @listaAnimesFiltrada2.slice 0, @numeroDeAnimes
+
 
 #
 </script>
@@ -41,14 +56,23 @@
 
     .gridIn
         display: grid
-        grid-template-columns: repeat(4, 21.25%)
-        grid-column-gap: 5%
+        grid-template-columns: repeat(5, 18.4%)
+        grid-column-gap: 2%
         grid-row-gap: 1.5rem
+        grid-template-rows: repeat(2, auto)
+        overflow: hidden
+
+
+    @media only screen and (max-width: 1440px)
+        .gridIn
+            grid-template-columns: repeat(4, 21.25%)
+            grid-column-gap: 5%
+
 
     @media only screen and (max-width: 500px)
         .gridIn
-            grid-template-columns: repeat(2, 1fr)
-            grid-gap: 1rem
+            grid-template-columns: repeat(2, 48%)
+            grid-column-gap: 4%
 
     //
 </style>
