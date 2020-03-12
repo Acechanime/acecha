a4f814293b228d5884bd7c4b21ef04987372e546
Araozu <tbyuyfcjjll@gmail.com>
2020-03-12 17:28:02 -0500

    Revertido orden de los episodios a descendente.

--- src/components/Anime/lista-de-episodios.vue
+++ src/components/Anime/lista-de-episodios.vue
@@ -41,15 +41,15 @@
             episodios_filtrados: ->
                 epsFiltrados = @episodios.filter (x) -> x.es_ova is no
                 epsFiltrados.sort (x, y) =>
-                    if x.num_ep > y.num_ep then 1
-                    else -1
+                    if x.num_ep > y.num_ep then -1
+                    else 1
 
 
             ovas_filtradas: ->
                 epsFiltrados = @episodios.filter (x) -> x.es_ova is yes
                 epsFiltrados.sort (x, y) =>
-                    if x.num_ep > y.num_ep then 1
-                    else -1
+                    if x.num_ep > y.num_ep then -1
+                    else 1
 
 
             tieneOvas: -> @ovas_filtradas.length > 0
