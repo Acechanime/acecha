87a3a7776c95d279f95f20bda1b0841adbd1e4d6
Araozu (woo) <faraoz@unsa.edu.pe>
2020-04-30 18:43:03 -0500

    Agregada tolerancia a fechas de inicio vacias al calendario.

--- src/components/App/indicador-version.vue
+++ src/components/App/indicador-version.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     div.beta(v-if="mostrarVersion || esBeta")
-        span v 4.6.1{{ esBeta? '-beta': '' }}
+        span v 4.6.2{{ esBeta? '-beta': '' }}
 
     //
 </template>
