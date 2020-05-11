10dbcc26f91631c7bff5012f4bff46060c6ae864
Araozu <tbyuyfcjjll@gmail.com>
2020-05-11 15:56:27 -0500

    Publicidad a producción.

--- src/assets/ads/ads.js
+++ src/assets/ads/ads.js
@@ -2,29 +2,10 @@ export const cargarAds = player => {
 
     const options = {
         id: "content_video",
-        adTagUrl: "http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator="
+        adTagUrl: BB.getVASTUrl(2012102)
     };
 
     player.ima(options);
 
-    const contentPlayer =  document.getElementById('content_video_html5_api');
-    if ((navigator.userAgent.match(/iPad/i) ||
-        navigator.userAgent.match(/Android/i)) &&
-        contentPlayer &&
-        contentPlayer.hasAttribute('controls')) {
-        contentPlayer.removeAttribute('controls');
-    }
-    let startEvent = 'click';
-    if (navigator.userAgent.match(/iPhone/i) ||
-        navigator.userAgent.match(/iPad/i) ||
-        navigator.userAgent.match(/Android/i)) {
-        startEvent = 'touchend';
-    }
-
-    player.one(startEvent, function() {
-        player.ima.initializeAdDisplayContainer();
-        player.ima.requestAds();
-        player.play();
-    });
 };
 
