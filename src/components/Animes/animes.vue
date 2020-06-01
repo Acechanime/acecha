e8bae2e22a63ce0c785b873b7a0cf13ca8dbae3f
Araozu (Win) <faraozu@unsa.edu.pe>
2020-06-01 11:09:24 -0500

    Agregada paginación a la lista de animes

--- src/components/Animes/animes.vue
+++ src/components/Animes/animes.vue
@@ -1,8 +1,23 @@
 <template lang="pug">
     div
-        // p {{ numeroDeAnimes }}
+        div.paginacion
+            template(v-for="n in paginasEnMedio")
+                template(v-if="n === -1")
+                    span.rango ...
+                template(v-else)
+                    span(:class="pagina === n? 'activo': ''" @click="cambiarPagina(n)")
+                        | {{ n + 1 }}
+
         div.gridIn
-            anime(v-for="anime in listaAnimesFiltrada2" :key="anime.anime_id" :anime="anime")
+            anime(v-for="anime in listaAnimesFiltrada3" :key="anime.anime_id" :anime="anime")
+
+        div.paginacion
+            template(v-for="n in paginasEnMedio")
+                template(v-if="n === -1")
+                    span.rango ...
+                template(v-else)
+                    span(:class="pagina === n? 'activo': ''" @click="cambiarPagina(n)")
+                        | {{ n + 1 }}
 
     //
 </template>
@@ -11,12 +26,14 @@
     import anime from "./anime.vue"
     import { comp, filtrar } from "../../coffee/buscador.coffee"
 
-    numeroDeFilas = 4
+    numeroDeFilas = 5
 
     export default
         name: "animes"
         components:
             anime: anime
+        data: ->
+            pagina: 0
         props:
             filtro:
                 type: Function # (Contexto, Anime) -> Prioridad
@@ -31,7 +48,6 @@
             listaAnimes: -> @$store.state.datos.animes
 
             listaAnimesFiltrada2: ->
-                # @terminarCargaFn()
                 primerRes = filtrar @, @listaAnimes, @filtro
                 primerRes.sort @funOrden
 
@@ -49,14 +65,58 @@
                     else 2
                 numeroDeFilas * numeroDeColumnas
 
-            listaAnimesFiltrada3: -> @listaAnimesFiltrada2.slice 0, @numeroDeAnimes
+            numeroDePaginas: ->
+                Math.ceil(@listaAnimesFiltrada2.length / @numeroDeAnimes)
 
+            paginasEnMedio: ->
+                pag = @pagina
+                numP = @numeroDePaginas
+                if numP > 6
+                    switch pag
+                        when 0, 1, 2 then [0, 1, 2, 3, -1, numP - 1]
+                        when numP, numP - 1, numP - 2 then [0, -1, numP - 3, numP - 2, numP - 1]
+                        else
+                            [0, -1, pag - 1, pag, pag + 1 , -1, numP - 1]
+                else
+                    for i in [0 ... numP]
+                        i
+
+            listaAnimesFiltrada3: ->
+                if @pagina > @numeroDePaginas
+                    @pagina = @numeroDePaginas
+                else if @pagina < 0
+                    @pagina = 0
+                else
+                    posInicial = @pagina * @numeroDeAnimes
+                    posFinal = posInicial + @numeroDeAnimes
+                    @listaAnimesFiltrada2.slice posInicial, posFinal
+        methods:
+            cambiarPagina: (n) ->
+                @pagina = n
 
 #
 </script>
 
 <style scoped lang="sass">
     @import "../../sass/variables"
+
+    .paginacion
+        text-align: center
+        padding: 1rem 0
+        span
+            display: inline-block
+            padding: 0.5rem 0
+            border: solid 1px var(--colorPrincipal)
+            text-align: center
+            width: 2rem
+            cursor: pointer
+            &.rango
+                cursor: default
+            &.activo
+                background-color: var(--colorPrincipal)
+                color: white
+
+
 
     .gridIn
         display: grid
--- src/components/App/indicador-version.vue
+++ src/components/App/indicador-version.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     div.beta(v-if="mostrarVersion || esBeta")
-        span v 4.10.1{{ esBeta? '-beta': '' }}
+        span v 4.11.0{{ esBeta? '-beta': '' }}
 
     //
 </template>
